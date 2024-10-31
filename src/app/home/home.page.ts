b205307d87f2fd8c9eb704f8366ab9becff215db
Alaric <ckagenou96@gmail.com>
2024-10-31 13:24:05 +0300

    base created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonAlert } from '@ionic/angular/standalone';
+import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputTextModule } from "primeng/inputtext";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonAlert, IonHeader, IonToolbar, IonTitle, IonContent, CardModule, ButtonModule,
+    InputGroupModule, InputTextModule
+   ],
 })
 export class HomePage {
+  public alertButtons = ['OK'];
   constructor() {}
+
+
 }
